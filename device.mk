23823a0e6c5dc5efd6b203966121a2a062a026e2
Alin Jerpelea <alin.jerpelea@sony.com>
2018-04-20 10:21:44 +0200

    add TouchDetector.idc
    Xperia Touch uses TouchDetector.idc for the touch input
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/etc/acdbdata/Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Headset_cal.acdb \
     $(DEVICE_PATH)/vendor/etc/acdbdata/Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Speaker_cal.acdb
 
+# Touch IDC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/usr/idc/TouchDetector.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/TouchDetector.idc
+
 # NFC Configuration
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/etc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
---
+++ rootdir/vendor/usr/idc/TouchDetector.idc
@@ -0,0 +1,6 @@
+# Copyright (c) 2017 Sony Mobile Communications Inc.
+# All rights, including trade secret rights, reserved.
+
+# Device Type
+touch.deviceType = touchScreen
+touch.orientationAware = 1
